f833e1fb6c8697085bcd5e255f43100f4457de2a
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-07-19 10:36:57 -0700

    connected to MongoDB

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,4 +1,5 @@
-
+require('../models/database');
+const Category = require('../models/Category');
 
 
 // * Get /
@@ -7,3 +8,41 @@
 exports.homepage = async(req, res) => {
     res.render('index', { title: 'Recipe Blog - Home'});
 }
+
+
+// connected and working
+// async function insertCategoryData(){
+
+//     try {
+//         await Category.insertMany([
+//             {
+//                 "name": "Thai",
+//                 "image": "thai-food.jpeg"
+//             },
+//             {
+//                 "name": "American",
+//                 "image": "american-food.jpeg"
+//             },
+//             {
+//                 "name": "Chinese",
+//                 "image": "chinese-food.jpeg"
+//             },
+//             {
+//                 "name": "Mexican",
+//                 "image": "mexican-food.jpeg"
+//             },
+//             {
+//                 "name": "Indian",
+//                 "image": "indian-food.jpeg"
+//             },
+//             {
+//                 "name": "Spanish",
+//                 "image": "spanish-food.jpeg"
+//             },
+//         ]);
+//     } catch (error) {
+//     console.log('err', + error)
+//     }
+// }
+
+insertCategoryData();
