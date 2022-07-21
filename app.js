f785e4b14b512028c1e96e7a2b79d3dcb9530a55
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-07-20 19:56:41 -0700

    starting over

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// dependencies
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
 require('dotenv').config();
+//require('express-async-errors');
+// dependencies
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+
+
+
 
 // MIDDLEWARE
 app.use(express.urlencoded( { extended: true } ));
@@ -15,7 +17,8 @@ app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-const routes = require('./server/routes/recipeRoutes.js')
-app.use('/', routes);
+const recipeRoutes = require('./server/routes/recipe-routes')
+app.use('/', recipeRoutes);
 
+const port = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, () => console.log(`listening to port ${port}`))
